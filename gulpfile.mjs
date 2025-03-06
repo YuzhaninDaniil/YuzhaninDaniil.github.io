03594bcb95b3e8eb26b14310714705723d8b43d2
danYuzh05 <weltrups@gmail.com>
2025-03-06 20:01:09 +0700

    final04ka

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -26,15 +26,14 @@ const resources = {
   svgSprite: "src/assets/svg-sprite/*.svg",
   static: [
     "src/assets/icons/**/*.*",
-    // "src/assets/favicons/**/*.*",
+    "src/assets/favicons/**/*.*",
     "src/assets/fonts/**/*.{woff,woff2}",
-    // "src/assets/video/**/*.{mp4,webm}",
-    // "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
-    // "src/json/**/*.json",
-    // "src/php/**/*.php"
+    "src/assets/video/**/*.{mp4,webm}",
+    "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
+    "src/json/**/*.json",
+    "src/php/**/*.php"
   ]
 };
-// Gulp Tasks:
 function clean() {
   return del("dist");
 }
